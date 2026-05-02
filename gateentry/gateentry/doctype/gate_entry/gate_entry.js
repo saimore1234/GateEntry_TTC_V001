5f01de8bb4e8e560c3abe9b05866d6ad5c5d0410
saimore1234 <moresai881@gmai.com>
2026-05-02 20:22:08 +0530

    Gate Entry App

--- gateentry/gateentry/doctype/gate_entry/gate_entry.js
+++ gateentry/gateentry/doctype/gate_entry/gate_entry.js
@@ -1,14 +1,13 @@
 frappe.ui.form.on('Gate Entry', {
     refresh: function(frm) {
-        // Add custom buttons
         if (frm.doc.docstatus === 0) {
             frm.add_custom_button(__('Select POs'), function() {
-                frm.trigger('select_pos');
+                frm.trigger('select_purchase_orders');
             }).addClass('btn-primary');
         }
     },
     
-    select_pos: function(frm) {
+    select_purchase_orders: function(frm) {
         // Get open POs
         frappe.call({
             method: "frappe.client.get_list",
@@ -18,22 +17,21 @@ frappe.ui.form.on('Gate Entry', {
                     docstatus: 1,
                     status: ["in", ["To Receive and Bill", "To Receive"]]
                 },
-                fields: ["name", "supplier", "supplier_name", "transaction_date", "total_qty"],
+                fields: ["name", "supplier", "transaction_date"],
                 limit_page_length: 100
             },
             callback: function(r) {
                 if (r.message && r.message.length > 0) {
-                    let pos = r.message;
                     let dialog = new frappe.ui.Dialog({
                         title: 'Select Purchase Orders',
                         fields: [
                             {
-                                fieldname: 'po_list',
+                                fieldname: 'po_html',
                                 fieldtype: 'HTML',
-                                options: '<div id="po_checkbox_list"></div>'
+                                options: '<div id="po_list"></div>'
                             }
                         ],
-                        primary_action_label: 'Add Selected POs',
+                        primary_action_label: 'Load Items',
                         primary_action: function(values) {
                             let selected = [];
                             $('.po-checkbox:checked').each(function() {
@@ -45,14 +43,21 @@ frappe.ui.form.on('Gate Entry', {
                                 return;
                             }
                             
-                            frm.set_value('selected_pos', selected.join(', '));
-                            dialog.hide();
+                            frappe.show_alert({
+                                message: __('Loading items from selected POs...'),
+                                indicator: 'orange'
+                            });
                             
-                            // Auto fetch items
-                            frm.call('fetch_items').then(() => {
+                            frm.call('add_purchase_orders', {
+                                po_names: JSON.stringify(selected)
+                            }).then(() => {
+                                frm.refresh_field('purchase_orders');
                                 frm.refresh_field('items');
+                                frm.refresh_field('total_qty');
+                                frm.refresh_field('total_weight');
+                                dialog.hide();
                                 frappe.show_alert({
-                                    message: __('Items loaded successfully'),
+                                    message: __('Items loaded successfully!'),
                                     indicator: 'green'
                                 });
                             });
@@ -66,27 +71,25 @@ frappe.ui.form.on('Gate Entry', {
                     html += '<th>PO Number</th>';
                     html += '<th>Supplier</th>';
                     html += '<th>Date</th>';
-                    html += '<th>Total Qty</th>';
                     html += '</tr></thead><tbody>';
                     
-                    $.each(pos, function(i, po) {
+                    $.each(r.message, function(i, po) {
                         html += '<tr>';
                         html += '<td><input type="checkbox" class="po-checkbox" value="' + po.name + '"></td>';
                         html += '<td>' + po.name + '</td>';
-                        html += '<td>' + (po.supplier_name || po.supplier) + '</td>';
+                        html += '<td>' + (po.supplier || '') + '</td>';
                         html += '<td>' + po.transaction_date + '</td>';
-                        html += '<td>' + (po.total_qty || 0) + '</td>';
                         html += '</tr>';
                     });
                     
                     html += '</tbody></table></div>';
-                    dialog.fields_dict.po_list.$wrapper.html(html);
+                    html += '<script>';
+                    html += '$("#select_all").on("change", function() {';
+                    html += '    $(".po-checkbox").prop("checked", $(this).prop("checked"));';
+                    html += '});';
+                    html += '</script>';
                     
-                    // Select all functionality
-                    $('#select_all').on('change', function() {
-                        $('.po-checkbox').prop('checked', $(this).prop('checked'));
-                    });
-                    
+                    dialog.fields_dict.po_html.$wrapper.html(html);
                     dialog.show();
                 } else {
                     frappe.msgprint(__('No open Purchase Orders found'));
@@ -95,14 +98,15 @@ frappe.ui.form.on('Gate Entry', {
         });
     },
     
-    gate_in_time: function(frm) {
-        if (!frm.doc.gate_in_time) {
-            frm.set_value('gate_in_time', frappe.datetime.now_datetime());
+    validate: function(frm) {
+        // Check if items exist
+        if (frm.doc.items && frm.doc.items.length === 0) {
+            frappe.throw(__('Please select Purchase Orders first'));
         }
     }
 });
 
-// Handle items table events
+// Handle received quantity changes
 frappe.ui.form.on('Gate Entry Item', {
     received_qty: function(frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
@@ -113,11 +117,7 @@ frappe.ui.form.on('Gate Entry Item', {
         update_summary(frm);
     },
     
-    no_of_packages: function(frm) {
-        update_summary(frm);
-    },
-    
-    total_weight: function(frm) {
+    weight: function(frm) {
         update_summary(frm);
     },
     
@@ -128,16 +128,13 @@ frappe.ui.form.on('Gate Entry Item', {
 
 function update_summary(frm) {
     let total_qty = 0;
-    let total_pkgs = 0;
     let total_wt = 0;
     
     $.each(frm.doc.items || [], function(i, item) {
         total_qty += flt(item.received_qty);
-        total_pkgs += flt(item.no_of_packages);
-        total_wt += flt(item.total_weight);
+        total_wt += flt(item.weight);
     });
     
-    frm.set_value('total_quantity', total_qty);
-    frm.set_value('total_packages', total_pkgs);
+    frm.set_value('total_qty', total_qty);
     frm.set_value('total_weight', total_wt);
 }
--- gateentry/gateentry/doctype/gate_entry/gate_entry.py
+++ gateentry/gateentry/doctype/gate_entry/gate_entry.py
@@ -1,64 +1,51 @@
 import frappe
 from frappe.model.document import Document
-from frappe.utils import flt, now_datetime
+from frappe.utils import flt
 from frappe import _
 
 class GateEntry(Document):
     
     def validate(self):
         self.calculate_totals()
-        self.validate_quantities()
     
     def calculate_totals(self):
         total_qty = 0
-        total_pkgs = 0
         total_wt = 0
-        
         for item in self.items:
             total_qty += flt(item.received_qty)
-            total_pkgs += flt(item.no_of_packages or 0)
-            total_wt += flt(item.total_weight or 0)
-        
-        self.total_quantity = total_qty
-        self.total_packages = total_pkgs
+            total_wt += flt(item.weight or 0)
+        self.total_qty = total_qty
         self.total_weight = total_wt
     
-    def validate_quantities(self):
-        for item in self.items:
-            if item.received_qty > item.ordered_qty:
-                frappe.throw(_("Row #{0}: Received Qty cannot exceed Ordered Qty for {1}").format(
-                    item.idx, item.item_code))
-    
     @frappe.whitelist()
-    def fetch_items(self):
-        """Fetch items from selected POs"""
-        if not self.selected_pos:
-            frappe.throw(_("Please select Purchase Orders first using the 'Select Purchase Orders' button"))
+    def add_purchase_orders(self, po_names):
+        """Add POs and auto-fetch items"""
+        import json
+        po_list = json.loads(po_names)
         
-        po_names = [po.strip() for po in self.selected_pos.split(',') if po.strip()]
-        
-        if not po_names:
-            frappe.throw(_("No valid POs found"))
-        
-        # Clear existing items
+        # Clear existing
+        self.purchase_orders = []
         self.items = []
         suppliers = set()
         
-        # Fetch items from each PO
-        for po_name in po_names:
+        for po_name in po_list:
             if not frappe.db.exists("Purchase Order", po_name):
-                frappe.msgprint(_("Warning: PO {0} not found, skipping").format(po_name))
                 continue
             
-            po = frappe.get_doc("Purchase Order", po_name)
-            suppliers.add(po.supplier)
+            po_doc = frappe.get_doc("Purchase Order", po_name)
+            suppliers.add(po_doc.supplier)
             
-            po_items = frappe.get_all("Purchase Order Item",
-                filters={"parent": po_name},
-                fields=["item_code", "item_name", "description", "qty", "uom"]
-            )
+            # Add to PO Tracking table
+            self.append("purchase_orders", {
+                "purchase_order": po_name,
+                "supplier": po_doc.supplier,
+                "order_date": po_doc.transaction_date,
+                "total_qty": sum(item.qty for item in po_doc.items),
+                "status": "Pending"
+            })
             
-            for po_item in po_items:
+            # Auto-fetch items from PO
+            for po_item in po_doc.items:
                 # Check if item already exists from another PO
                 existing = None
                 for item in self.items:
@@ -71,57 +58,27 @@ class GateEntry(Document):
                     existing.received_qty += po_item.qty
                 else:
                     self.append("items", {
-                        "purchase_order": po_name,
+                        "purchase_order_ref": po_name,
                         "item_code": po_item.item_code,
                         "item_name": po_item.item_name,
-                        "description": po_item.description,
                         "ordered_qty": po_item.qty,
                         "received_qty": po_item.qty,
                         "uom": po_item.uom,
-                        "quality_status": "Pending",
-                        "no_of_packages": 1
+                        "quality": "Pending",
+                        "packages": 1,
+                        "weight": 0
                     })
         
-        # Set supplier info if single supplier
+        # Set supplier if all POs have same supplier
         if len(suppliers) == 1:
             supplier = list(suppliers)[0]
             self.supplier = supplier
             self.supplier_name = frappe.db.get_value("Supplier", supplier, "supplier_name")
         
         self.calculate_totals()
-        frappe.msgprint(_("Loaded {0} items from {1} PO(s)").format(len(self.items), len(po_names)))
-        return self.items
+        frappe.msgprint(_("Added {0} PO(s) and loaded {1} items").format(len(po_list), len(self.items)))
+        return True
     
     def on_submit(self):
-        self.update_purchase_orders()
         self.db_set("status", "Completed")
-    
-    def update_purchase_orders(self):
-        """Update each Purchase Order"""
-        po_updates = {}
-        
-        for item in self.items:
-            if item.purchase_order:
-                if item.purchase_order not in po_updates:
-                    po_updates[item.purchase_order] = []
-                po_updates[item.purchase_order].append(item)
-        
-        for po_name, items in po_updates.items():
-            po = frappe.get_doc("Purchase Order", po_name)
-            
-            for po_item in po.items:
-                for gate_item in items:
-                    if po_item.item_code == gate_item.item_code:
-                        current_received = flt(po_item.get("received_qty_at_gate", 0))
-                        new_received = current_received + flt(gate_item.received_qty)
-                        po_item.db_set("received_qty_at_gate", new_received, update_modified=False)
-                        po_item.db_set("gate_entry_reference", self.name, update_modified=False)
-                        
-                        if new_received >= flt(po_item.qty):
-                            po_item.db_set("gate_entry_status", "Completed", update_modified=False)
-                        elif new_received > 0:
-                            po_item.db_set("gate_entry_status", "Partial", update_modified=False)
-            
-            frappe.db.commit()
-        
-        frappe.msgprint(_("Updated {0} Purchase Order(s)").format(len(po_updates)))
+        frappe.msgprint(_("Gate Entry {0} submitted successfully").format(self.name))
